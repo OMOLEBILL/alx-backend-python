a01587d73464c655c2f20136c0978acffee9c9c2
bill omole <omolebill01@gmail.com>
2023-04-27 22:47:08 +0200

    7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" We combine List and Tuples from the typing module """
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ We return a tuple from the given arguements """
+    return (k, v)
