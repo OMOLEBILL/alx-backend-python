eb22f24ef6c9f0b88e8a75f7f89e9359d98835aa
bill omole <omolebill01@gmail.com>
2023-04-27 13:19:45 +0200

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+""" implimenting types in this module """
+
+
+def sum_list(input_list :List[float]) -> float:
+    """ we sum the floats in a list """
+    return sum(input_list)
