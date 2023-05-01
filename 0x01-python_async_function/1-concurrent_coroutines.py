e81805324da031fcb4ee6c0e5a461a7f8e176350
bill omole <omolebill01@gmail.com>
2023-05-01 19:45:22 +0200

    1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" We span the imported function n times """
+import random
+import asyncio
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns wait_random n times with the specified max_delay
+    and returns a list of all the delays
+    """
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    delays = []
+    tasks = []
+    for _ in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        tasks.append(task)
+    for task in tasks:
+        delay = await task
+        delays.append(delay)
+    delays.sort()
+    return delays
