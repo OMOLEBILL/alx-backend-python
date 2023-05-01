c90b593187cfd56aa2a94730ac6729ec8904e325
bill omole <omolebill01@gmail.com>
2023-05-01 20:31:49 +0200

    completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,26 +1,7 @@
 #!/usr/bin/env python3
 """ We span the imported function n times """
-import random
 import asyncio
 from typing import List
-
-
-def bubble_sort(lists: List[float]) -> List[float]:
-    """ We use buuble sort to sort the list """
-    n = len(lists)
-    for i in range(n):
-        # Flag to check if any swapping happened in this iteration
-        swapped = False
-        for j in range(n - i - 1):
-            if lists[j] > lists[j + 1]:
-                # Swap adjacent elements
-                lists[j], lists[j + 1] = lists[j + 1], lists[j]
-                # Set swapped flag to true
-                swapped = True
-        # If no swapping happened in this iteration, the list is already sorted
-        if not swapped:
-            break
-    return lists
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
@@ -33,8 +14,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for _ in range(n):
         task = asyncio.create_task(wait_random(max_delay))
         tasks.append(task)
-    for task in tasks:
+    for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
-    sortdelay = bubble_sort(delays)
-    return sortdelay
+    return delays
