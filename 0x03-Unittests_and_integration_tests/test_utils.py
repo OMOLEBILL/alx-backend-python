cb4bd9c77f7e5ab2d4eca87f7fdbd8a2f35ff1c7
bill omole <omolebill01@gmail.com>
2023-05-21 11:52:09 +0200

    test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" We test the access_nested_map method from the utils
+    module
+"""
+import unittest
+from parameterized import parameterized
+from typing import (
+    Mapping,
+    Sequence,
+    Any
+)
+access_nested_map = __import__("utils").access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ We implement a base class to test the utils class"""
+    @parameterized.expand([
+        ({"a": 1}, ("a"), 1),
+        ({"a": {"b": 2}}, ("a"), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map: Mapping, path:
+                               Sequence, expected: Any):
+        """WE test the access_nested_map
+        args:
+            nested_map : the nested dictionary
+            path: the sequence of the keys to the dictionary
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
