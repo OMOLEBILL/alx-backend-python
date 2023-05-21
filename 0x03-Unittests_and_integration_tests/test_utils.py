926d5fa419fd039ba4c3bb015270753279d1084c
bill omole <omolebill01@gmail.com>
2023-05-21 19:00:26 +0200

    mocked and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,13 +3,16 @@
     module
 """
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 from typing import (
     Mapping,
     Sequence,
+    Dict,
     Any
 )
 access_nested_map = __import__("utils").access_nested_map
+get_json = __import__("utils").get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -41,3 +44,22 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    """ We test the get_json method from the utils module"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io",{"payload": False})
+    ])
+    def test_get_json(self, test_url:str, test_payload:Dict):
+        """ We test the get_json method
+            args:
+                @test_url the unique resource locator
+                @test_payload the expected dictionary
+        """
+        mocked = Mock()
+        mocked.json.return_value = test_payload
+        with patch("requests.get", return_value=mocked) as mock_request:
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
+            mock_request.assert_called_once()
