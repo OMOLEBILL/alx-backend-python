10e47c43402fa2f8800d39ba3be28554b9112e67
bill omole <omolebill01@gmail.com>
2023-05-21 21:38:56 +0200

    added

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,3 +34,13 @@ class TestGithubOrgClient(TestCase):
                    PropertyMock(return_value=diction)):
             Instance = GithubOrgClient("added")
             self.assertEqual(Instance._public_repos_url, expected)
+
+    @patch("client.get_json")
+    def test_public_repos(self, Mocked: Mock):
+        """ We test the public_repos"""
+        Mocked.return_value = {"repos_url": {"name": "fatal not a git repo"}}
+        result = "fatal not a git repo"
+        with patch("client.GithubOrgClient._public_repos_url",
+                   PropertyMock(return_value=result)):
+            Instance = GithubOrgClient("added")
+            self.assertCountEqual(Instance._public_repos_url, result)
