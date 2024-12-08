c0be163c213f69e098533173ea32edad80de7046
omole otieno <omolebill01@gmail.com>
2024-12-08 11:03:05 +0100

    Appended a time to the logger

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,5 +1,6 @@
 import sqlite3
 import functools
+from datetime import datetime
 
 #### decorator to lof SQL queries
 def log_queries(func):
@@ -7,7 +8,7 @@ def log_queries(func):
     def wrapper(*args, **kwargs):
         query = kwargs.get('query', args[0] if args else None)
         if query:
-            print(query)
+            print(f"[{datetime.now()}] - Executing query: {query}")
         return func(*args, **kwargs)
     return wrapper
 
