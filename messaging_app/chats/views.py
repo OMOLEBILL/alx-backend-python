e31488b82ba3e0e91b231c0992745aa6af69d52c
omole otieno <omolebill01@gmail.com>
2024-12-24 13:07:05 +0100

    added the views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for listing, creating, retrieving, updating, or deleting conversations.
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for listing, creating, retrieving, updating, or deleting messages.
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+   
